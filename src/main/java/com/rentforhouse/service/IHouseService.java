b09b6ff918697ee29786bb347f3ed3a7c95f18ec
dinhtho2001 <phamdinhtho.0105@gmail.com>
2022-10-21 15:25:06 +0700

    update

--- src/main/java/com/rentforhouse/service/IHouseService.java
+++ src/main/java/com/rentforhouse/service/IHouseService.java
@@ -4,12 +4,12 @@ import org.springframework.data.domain.Pageable;
 
 import com.rentforhouse.dto.HouseDto;
 import com.rentforhouse.payload.request.HouseRequest;
-
+import com.rentforhouse.payload.request.HouseSaveRequest;
 import com.rentforhouse.payload.response.HouseResponse;
 
 public interface IHouseService {
 
 	HouseResponse findHouse(HouseRequest houseRequest ,Pageable pageable);
-	HouseDto saveHouse(HouseDto houseDto);
+	HouseDto saveHouse(HouseSaveRequest houseSaveRequest);
 	HouseDto findById(Long id);
 }
