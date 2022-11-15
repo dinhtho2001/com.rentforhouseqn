59652b45f4dbf2d47e53429496b9156aca760d93
dinhtho2001 <phamdinhtho.0105@gmail.com>
2022-11-15 19:56:46 +0700

    fix//

--- src/main/java/com/rentforhouse/config/security/SecurityConfig.java
+++ src/main/java/com/rentforhouse/config/security/SecurityConfig.java
@@ -63,8 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		web.ignoring().antMatchers("/swagger-ui.html").antMatchers("/webjars/springfox-swagger-ui/**")
 				.antMatchers("/swagger-resources/**").antMatchers("/v2/api-docs")
 				.antMatchers("/api/auth/**")
-				.antMatchers(HttpMethod.GET, "/api/houses/**", "/api/houseTypes")
-				.antMatchers(HttpMethod.GET, "/api/comments/**");
+				.antMatchers(HttpMethod.GET, "/api/houses/**", "/api/houseTypes", "/api/comments/**");
 	}
 
 	@Bean
